643e993fe7a93880d872dc737a90dd5d9f54ab4f
Seonyul Park <seonyulpark@Seonyului-MacBookAir.local>
2019-06-06 13:50:26 +0900

    todo component 추가

---
+++ src/components/Header.vue
@@ -0,0 +1,46 @@
+<template>
+  <header class="header">
+    <h1>todos</h1>
+    <input
+      class="new-todo"
+      v-model="text"
+      autofocus
+      autocomplete="off"
+      placeholder="What needs to be done?"
+      @keypress="handleInput"
+    >
+  </header>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      text: ""
+    };
+  },
+  methods: {
+    handleInput(e) {
+      if (e.keyCode === 13) {
+        this.$emit("addTodo", this.text);
+        this.text = "";
+      }
+    }
+  }
+};
+</script>
+
+<style>
+h1 {
+  position: absolute;
+  top: -155px;
+  width: 100%;
+  font-size: 100px;
+  font-weight: 100;
+  text-align: center;
+  color: rgba(175, 47, 47, 0.15);
+  -webkit-text-rendering: optimizeLegibility;
+  -moz-text-rendering: optimizeLegibility;
+  text-rendering: optimizeLegibility;
+}
+</style>
--- src/views/Todo.vue
+++ src/views/Todo.vue
@@ -1,13 +1,79 @@
 <template>
-    <div>Todo :)</div>
+  <div id="app">
+    <section class="todoapp">
+      <Header @addTodo="addTodo"/>
+      <Body :todos="filteredTodos" @updateTodo="updateTodo" @deleteTodo="deleteTodo"/>
+      <Footer :size="size" @onChangeFilter="handleFilter"/>
+    </section>
+  </div>
 </template>
 
 <script>
-export default {
+import "../assets/css/main.css";
 
-}
+import Header from "../components/Header";
+import Body from "../components/Body";
+import Footer from "../components/Footer";
+
+export default {
+  components: {
+    Header,
+    Body,
+    Footer
+  },
+  data() {
+    return {
+      todos: [],
+      filterType: "All"
+    };
+  },
+  computed: {
+    size() {
+      return this.filteredTodos.length;
+    },
+    filteredTodos() {
+      switch (this.filterType) {
+        case "Doing": {
+          return this.todos.filter(todo => !todo.isDone);
+        }
+        case "Done": {
+          return this.todos.filter(todo => todo.isDone);
+        }
+        default: {
+          return this.todos;
+        }
+      }
+    }
+  },
+  methods: {
+    addTodo(text) {
+      this.todos = [
+        ...this.todos,
+        {
+          key: new Date(),
+          text,
+          isDone: false
+        }
+      ];
+    },
+    updateTodo(key) {
+      const current = [...this.todos];
+      const todo = current.find(todo => todo.key === key);
+
+      if (todo) {
+        todo.isDone = !todo.isDone;
+        this.todos = current;
+      }
+    },
+    deleteTodo(key) {
+      this.todos = this.todos.filter(todo => todo.key !== key);
+    },
+    handleFilter(type) {
+      this.filterType = type;
+    }
+  }
+};
 </script>
 
 <style>
-
 </style>
